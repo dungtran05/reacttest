cb4d95233228e141328e72dde7da0689627f4cd6
dungtran05 <dungpk998@gmail.com>
2024-03-27 09:10:07 +0700

    apiuser

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,13 +1,38 @@
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import React from 'react'
 
 function Home() {
-    const res =  axios.get('https://localhost:7214/api/Users');
+  const [listdata, setListData] = useState([]);
+
+  useEffect(() => {
+    // Function to fetch data
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://localhost:7214/api/Users');
+        // Update state with fetched data
+        setListData(response.data);
+        console.log(listdata);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    // Call the fetchData function when the component mounts
+    fetchData();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // Empty dependency array ensures this effect runs once on component mount
+
   return (
-    <div>
-      {JSON.parse(res)}
-    </div>
-  )
+    <>
+      {listdata.map((item, index) => (
+        <div key={index}>
+         
+          {item.userName} 
+        </div>
+      ))}
+    </>
+  );
 }
 
-export default Home
+export default Home;
